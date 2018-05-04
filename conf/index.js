89253c21bbb314f7b1deccf73104c9c686f439c0
lihaipeng <759539395@qq.com>
2018-05-04 16:35:27 +0800

    update 更换redis

--- conf/index.js
+++ conf/index.js
@@ -6,12 +6,12 @@ const config = {
     port: '8666',
     webhost: 'http://dwz.loobool.com/',
     redis: {
-      host: "jredis-gz1-prod-redis-9ycerxf6f1.jmiss.jcloud.com",
-      port: "6379",
-      password: "redis-9ycerxf6f1:Loobool2017",
-      db: 1,
-      socket_keepalive: false,
-      no_ready_check: false
+        host: "jredis-gz1-prod-redis-3e8glyxyyu.jmiss.jcloud.com",
+        port: "6379",
+        password: "redis-3e8glyxyyu:Htkj1357",
+        db: 13,
+        socket_keepalive: false,
+        no_ready_check: false
     }
 };
 
