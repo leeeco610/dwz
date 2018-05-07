115541bcacbdcb544ffbfb729c5034503ed450f3
lihaipeng <759539395@qq.com>
2018-05-07 15:30:21 +0800

    update redis 参数修改

--- conf/index.js
+++ conf/index.js
@@ -6,10 +6,10 @@ const config = {
     port: '9999',
     webhost: 'http://dwz.loobool.com/',
     redis: {
-        host: "jredis-gz1-prod-redis-3e8glyxyyu.jmiss.jcloud.com",
+        host: "127.0.0.1",
         port: "6379",
-        password: "redis-3e8glyxyyu:Htkj1357",
-        db: 13,
+        password: "redis-1234567890.",
+        db: 1,
         socket_keepalive: false,
         no_ready_check: false
     }
